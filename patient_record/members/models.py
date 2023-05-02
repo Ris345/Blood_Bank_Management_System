15afe351acfcca4205b0178c1ed0a3ca158fedc5
Dorjee <rishav345@gmail.com>
2023-05-02 15:30:43 -0400

    added new members, new table,

--- patient_record/members/models.py
+++ patient_record/members/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Member(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    phonenumber = models.IntegerField(null=True)
+    age = models.IntegerField(null=True)
+    blood_group = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    volume_of_blood_donated = models.CharField(max_length=255)
--- patient_record/members/views.py
+++ patient_record/members/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+# def members(request):
+#     return HttpResponse("Blood Work Project")
 
 def members(request):
-    return HttpResponse("Blood Work Project")
+  template = loader.get_template('index.html')
+  return HttpResponse(template.render())
