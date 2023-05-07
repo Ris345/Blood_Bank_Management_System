88e868de229061b4e9460ee017a808fc6f250b60
Dorjee <rishav345@gmail.com>
2023-05-07 19:15:07 -0400

    CRUD

--- patient_record/members/admin.py
+++ patient_record/members/admin.py
@@ -3,4 +3,8 @@ from .models import Member
 
 # Register your models here.
 
-admin.site.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("firstname", "lastname", "phonenumber","age", "blood_group", "address", "volume_of_blood_donated")
+  
+admin.site.register(Member, MemberAdmin)
+
--- patient_record/members/models.py
+++ patient_record/members/models.py
@@ -10,3 +10,7 @@ class Member(models.Model):
     blood_group = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     volume_of_blood_donated = models.CharField(max_length=255)
+
+
+def __str__(self):
+    return f"{self.firstname} {self.lastname}"
